e63b17c1a07b968d5ccf9405fa0a5ba60590db76
hitose kakazu <hkakazu@hitosenoMacBook-Air.local>
2022-10-22 19:48:07 +0900

    seedsにテストデータとseedsファイル入れた

---
+++ hasura/migrations/default/1666434975176_create_table_public_user/up.sql
@@ -0,0 +1,1 @@
+CREATE TABLE "public"."user" ("user_id" integer NOT NULL, "user_name" text NOT NULL, "LINE_id" integer NOT NULL, PRIMARY KEY ("user_id") );
---
+++ hasura/seeds/default/1666435200650_user_test_data.sql
@@ -0,0 +1,5 @@
+SET check_function_bodies = false;
+INSERT INTO public."user" (user_id, user_name, "LINE_id") VALUES (1, 'れい', 1);
+INSERT INTO public."user" (user_id, user_name, "LINE_id") VALUES (2, 'はると', 2);
+INSERT INTO public."user" (user_id, user_name, "LINE_id") VALUES (3, 'たける', 3);
+INSERT INTO public."user" (user_id, user_name, "LINE_id") VALUES (4, 'じろう', 4);
